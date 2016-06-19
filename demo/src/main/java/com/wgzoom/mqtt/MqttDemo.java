3f2ab45693bf4709eeba958da80537cae99d36f9
Lewis Deng <lewis.deng@qq.com>
2016-06-19 23:10:46 +0800

    correct the comment

--- demo/src/main/java/com/wgzoom/mqtt/MqttDemo.java
+++ demo/src/main/java/com/wgzoom/mqtt/MqttDemo.java
@@ -1,9 +1,7 @@
 package com.wgzoom.mqtt;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -13,7 +11,6 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-//import com.aliyun.openservice.ons.mqtt.tool.MacSignature;
 
 public class MqttDemo {
     public static void main(String[] args) throws IOException {
@@ -27,10 +24,7 @@ public class MqttDemo {
         acessKey = properties.getProperty("AccessKey");
         secretKey = properties.getProperty("SecretKey");
         final String topic = properties.getProperty("Topic"); 
-        //如果该设备需要接收点对点的推送，那么需要订阅二级topic，topic/p2p/，但凡以topic/p2p/为前缀的，都认为是点对点推送。
-        final String p2ptopic = topic+"/p2p/";
-        //同时订阅两个topic，一个是基于标准mqtt协议的发布订阅模式，一个是扩展的点对点推送模式。
-        final String[] topicFilters=new String[]{"topic1"};// {topic+"/notice/",p2ptopic};
+        final String[] topicFilters=new String[]{"topic1"};
         clientId = properties.getProperty("ConsumerId");
         //String sign;
         MemoryPersistence persistence = new MemoryPersistence();
@@ -38,7 +32,6 @@ public class MqttDemo {
             final MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
             final MqttConnectOptions connOpts = new MqttConnectOptions();
             System.out.println("Connecting to broker: " + broker);
-            //sign = MacSignature.macSignature(clientId.split("@@@")[0], secretKey);
             connOpts.setUserName(acessKey);
             connOpts.setServerURIs(new String[] { broker });
             connOpts.setPassword(secretKey.toCharArray());
@@ -83,16 +76,11 @@ public class MqttDemo {
             Thread.sleep(1000L);
             for (int i = 0; i < 3; i++) {
                 try {
-                    String scontent = new Date()+"MQTT Test body" + i;
+                    String scontent = new Date()+" MQTT Test body " + i;
                     final MqttMessage message = new MqttMessage(scontent.getBytes());
                     message.setQos(1);
                     message.setRetained(true);
-                    //System.out.println(i+" pushed at "+new Date()+" "+ scontent);
-                    //消息发送到某个主题topic，所有订阅这个topic的设备都能收到这个消息。遵循mqtt的发布订阅规范，topic也可以是多级topic。
-                    //除了点对点topic/p2p/这个前缀的，作为点对点发送的特殊topic，不遵循发布订阅模式。
                     sampleClient.publish(topic+"", message);
-                    //发送给指定设备，格式为topic/p2p/targetClientId, targetClientId的格式详见user.properties文件。
-                    //sampleClient.publish(p2ptopic+properties.getProperty("ConsumerId"), message);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -106,4 +94,3 @@ public class MqttDemo {
         }
     }
 }
-
